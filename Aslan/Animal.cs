c2b4b734d80e95b44dfdc5e0e6a2293997fd80fb
Elliott Lie <Elliott.DTK.Lie@lystkom.se>
2018-12-14 13:12:59 +0100

    commit

--- Aslan/Animal.cs
+++ Aslan/Animal.cs
@@ -6,6 +6,55 @@ namespace Aslan
 {
     class Animal
     {
+        public string name { get; set; }
+        public string sound { get; set; }
+        public Boolean canFly { get; set; }
+
+        public string Show()
+        {
+            if(canFly == true)
+            {
+                return ("Name: " + name + "Sound: " + sound + "can it fly: yes" );
+
+            }
+            else
+            {
+                return ("Name: " + name + "Sound: " + sound + "can it fly: No");
+            }
+        }
+    }
+
+    class Dog : Animal
+    {
+        public Dog(string Name)
+        {
+            name = Name;
+            sound = "Woff";
+            canFly = false;
+        }
 
     }
+
+    class Cat : Animal
+    {
+        public Cat(string Name)
+        {
+            name = Name;
+            sound = "mooo";
+            canFly = false;
+        }
+
+    }
+
+    class Bird : Animal
+    {
+        public Bird(string Name)
+        {
+            name = Name;
+            sound = "kvikkvik";
+            canFly = true;
+        }
+
+    }
+
 }
--- Aslan/Program.cs
+++ Aslan/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Aslan
 {
     class Program
     {
+        public static List<Animal> AnimalList = new List<Animal>();
+        private static readonly Random getrandom = new Random();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+
+                Console.WriteLine("Hello");
+                Console.WriteLine("1: Add animal");
+                Console.WriteLine("2: See animal list");
+                String tempStrAns = Console.ReadLine();
+
+                // if user wants to see the animal list
+                switch (tempStrAns)
+                {
+                    case "1":
+                        AddAnimal();
+                        break;
+                    case "2":
+                        foreach (var animal in AnimalList)
+                        {
+                            animal.Show();
+                        }
+                        Console.ReadLine();
+                        break;
+                }
+            }
+
+
+        }
+
+        public static void AddAnimal()
+        {
+            Console.WriteLine("name of animal");
+            string animalName = Console.ReadLine();
+            Console.WriteLine("what animal type is it? Dog, Cat or Bird ");
+            string animalType = Console.ReadLine();
+            Animal animal = new Animal();
+
+            if (animalType == "dog" && animalType == "Dog")
+            {
+                AnimalList.Add(new Dog(animalName));
+            }
+            else if (animalType == "cat" && animalType == "Cat")
+            {
+                AnimalList.Add(new Cat(animalName));
+            }
+            else if (animalType == "bird" && animalType == "Bird")
+            {
+                AnimalList.Add(new Dog(animalName));
+            }
+
+
         }
     }
+
 }
